2310918f091af43b00ef2c843e55361ab7ae4c85
OHsujin <ooooohsu@dialoguespace.co.kr>
2022-05-19 18:13:03 +0900

    20220519
    application.yml jpa 설정해야함

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -2,11 +2,19 @@ package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class JpashopApplication {
 
 	public static void main(String[] args) {
+		
+		Hello hello = new Hello();
+		hello.setData("test");
+		
+		String data = hello.getData();
+		System.out.println("test : " + data);
+		
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
