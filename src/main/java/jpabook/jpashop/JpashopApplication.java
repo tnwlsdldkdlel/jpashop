f88ce23263ffa3e08d5316a95194b56d3ac979f9
OHsujin 오수진 <82794340+ooooohsu@users.noreply.github.com>
2022-05-24 13:42:13 +0900

    20210523

--- src/main/java/jpabook/jpashop/JpashopApplication.java
+++ src/main/java/jpabook/jpashop/JpashopApplication.java
@@ -2,19 +2,11 @@ package jpabook.jpashop;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+@SpringBootApplication
 public class JpashopApplication {
 
 	public static void main(String[] args) {
-		
-		Hello hello = new Hello();
-		hello.setData("test");
-		
-		String data = hello.getData();
-		System.out.println("test : " + data);
-		
 		SpringApplication.run(JpashopApplication.class, args);
 	}
 
