18899508fdd273beed3336b56a44e99a194227a5
OHsujin 오수진 <67881808+tnwlsdldkdlel@users.noreply.github.com>
2022-05-27 10:41:45 +0900

    20220527

--- src/main/java/jpabook/jpashop/controller/OrderController.java
+++ src/main/java/jpabook/jpashop/controller/OrderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -51,4 +52,11 @@ public class OrderController {
 		
 		return "order/orderList";
 	}
+	
+	 @PostMapping(value = "/orders/{orderId}/cancel")
+	 public String cancleOrder(@PathVariable("orderId") Long orderId) {
+		 orderService.cancelOrder(orderId);
+		 
+		 return "redirect:/orders";
+	 }
 }
